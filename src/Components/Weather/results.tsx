cdf83838102f09ca690b2e8c0380d98dd2ce55dc
Taha Tahiri Jotey <53621588+TAHATJ48@users.noreply.github.com>
2022-12-21 05:16:47 +0100

    Update results.tsx

--- src/Components/Weather/results.tsx
+++ src/Components/Weather/results.tsx
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from "react";
-import { Link } from 'react-router-dom';
 
 const Weather = () => {
 
@@ -68,7 +67,7 @@ const Weather = () => {
                     {
                         results.map((result) => {
                             return (
-                                <div className="mb-4 max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700">
+                                <div className="mb-4 mx-6 p-6 bg-white border border-gray-200 rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700">
                                     <a href="#">
                                         <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{result.name}</h5>
                                     </a>
